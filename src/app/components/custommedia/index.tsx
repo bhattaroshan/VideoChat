1c9a08b33a423fe8bb297ccbf12b8fa5e73c39a5
roshanbhatta <bhattaroshan@gmail.com>
2024-04-01 00:00:09 +0545

    added control icons

--- src/app/components/custommedia/index.tsx
+++ src/app/components/custommedia/index.tsx
@@ -1,4 +1,8 @@
 'use client'
+import CameraIcon from "@/icons/camera";
+import MicIcon from "@/icons/mic";
+import NoCameraIcon from "@/icons/nocamera";
+import NoMicIcon from "@/icons/nomic";
 import { useEffect, useRef, useState } from "react"
 // import Peer from "peerjs";
 import {v4 as uuidv4} from 'uuid';
@@ -8,11 +12,12 @@ export default function CustomMedia(){
     const meRef = useRef<HTMLVideoElement|null>(null);
     const remoteRef = useRef<HTMLVideoElement|null>(null);
     const textRef = useRef<HTMLInputElement>(null);
-    // const [peerId, setPeerId] = useState('');
     const [myId,setMyId] = useState('');
     const peerInstance = useRef<any>(null);
-    const [enableVideo,setEnableVideo] =useState(true);
     const [state,setState] = useState<MediaStream|null>(null);
+
+    const [cameraOn,setCameraOn] = useState(true);
+    const [micOn,setMicOn] = useState(true);
 
     useEffect(()=>{
         
@@ -42,7 +47,7 @@ export default function CustomMedia(){
             peer.on('call', async (call) => {
                 console.log("calling...")
                 try{
-                    stream = await navigator.mediaDevices.getUserMedia({ video: enableVideo, audio: true });
+                    stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
                     setState(stream);
                     if(meRef.current){
                         meRef.current.srcObject = stream;
@@ -107,23 +112,57 @@ export default function CustomMedia(){
 
       function handleVideo(){
         if(state){
-            setEnableVideo(p=>!p);
-            state.getVideoTracks()[0].enabled = enableVideo;
+            //state.getVideoTracks()[0].enabled = enableVideo;
         }
       }
 
-    return <div className='flex flex-col w-screen h-screen justify-center items-center gap-4'>
-            <p>{myId}</p>
-            <div className='flex flex-col gap-4'>
-                <video ref={meRef} autoPlay muted={true} className='rounded-xl bg-blue-200 w-40 h-40 object-cover'/>
-                <video ref={remoteRef} autoPlay className='rounded-xl bg-blue-200 w-40 h-40 object-cover' />
-            </div>
+    function handleCameraClick(){
+        setCameraOn(p=>!p);
+    }
 
-            <div className='flex gap-4'>
+    function handleMicClick(){
+        setMicOn(p=>!p);
+    }
+
+    return <div className='flex flex-col gap-4 items-center w-screen h-screen'>
+        <div className='relative flex flex-col w-screen h-max-10/12 gap-4 items-center bg-black'>
+            <video ref={meRef} autoPlay muted={true} className='absolute left-4 top-4 rounded-xl bg-blue-200 w-1/5 h-1/3 object-cover z-[1000]'/>
+            <video ref={remoteRef} autoPlay className='relative bg-gray-400 w-screen md:w-8/12 h-auto object-cover' />
+            
+            <div className='absolute flex gap-4 left-1/2 bottom-6 -translate-x-1/2 '>
+                {
+                    cameraOn?
+                <div className='w-fit h-fit p-4 
+                                rounded-full border bg-blue-400 border-blue-500 cursor-pointer
+                                hover:bg-blue-300 group' onClick={handleCameraClick}>
+                    <CameraIcon className='bottom-4 w-8 h-8 text-blue-100 group-hover:text-white'/>
+                </div>:
+                <div className='w-fit h-fit p-4 
+                                rounded-full border bg-blue-400 border-blue-500 cursor-pointer
+                                hover:bg-blue-300 group' onClick={handleCameraClick}>
+                    <NoCameraIcon className='bottom-4 w-8 h-8 text-blue-100 group-hover:text-white'/>
+                </div>
+                }
+                {
+                    micOn?
+                <div className='w-fit h-fit p-4 
+                                rounded-full border bg-blue-400 border-blue-500 cursor-pointer
+                                hover:bg-blue-300 group' onClick={handleMicClick}>
+                    <MicIcon className='bottom-4 w-8 h-8 text-blue-100 group-hover:text-white'/>
+                </div>:
+                <div className='w-fit h-fit p-4 
+                                rounded-full border bg-blue-400 border-blue-500 cursor-pointer
+                                hover:bg-blue-300 group' onClick={handleMicClick}>
+                    <NoMicIcon className='bottom-4 w-8 h-8 text-blue-100 group-hover:text-white'/>
+                </div>
+                }
+            </div>
+        </div>
+            
+            <div className='flex gap-4 w-screen md:w-6/12'>
                 <input type='text' ref={textRef} className='border rounded'/>
                 <button className='bg-blue-400 text-white p-2' onClick={handleConnect}>Connect</button>
                 <button className='bg-blue-400 text-white p-2' onClick={handleVideo}>Toggle video</button>
             </div>
-            
     </div>
 }
